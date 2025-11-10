f87fe631654e1b9d78bc2905767d20294abcfe49
zhenyu.jiang <jiangzhenyu@keyfil.com>
2025-11-10 16:54:57 +0800

    feat: 迁移基础目录（assets/utils/eventbus/components/views/router/stores）
    - 拷贝资源与模块，保持原工程结构
    - 删除重复 router/index.js 并改写 TS 路由

---
+++ src/utils/Constants.js
@@ -0,0 +1,30 @@
+const ACTION_TYPE = {
+    UNKNOW: {
+        value: 0,
+        desc: "未知行为"
+    },
+    COMMENT_LIKE: {
+        value: 1,
+        desc: "评论喜欢点赞"
+    },
+    COMMENT_HATE: {
+        value: 2,
+        desc: "讨厌评论"
+    },
+    VIDEO_LIKE: {
+        value: 3,
+        desc: "视频点赞"
+    },
+    VIDEO_COLLECT: {
+        value: 4,
+        desc: "视频收藏"
+    },
+    VIDEO_COIN: {
+        value: 5,
+        desc: "视频投币"
+    }
+}
+
+export {
+    ACTION_TYPE
+}
