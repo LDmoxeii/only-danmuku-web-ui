7767a36a02ed2892737849e85c60cd22b4b13232
zhenyu.jiang <jiangzhenyu@keyfil.com>
2025-11-11 09:55:29 +0800

    feat: TS 化通用组件 Cover（使用 TS 脚本并规范 proxy 类型）

--- src/components/Cover.vue
+++ src/components/Cover.vue
@@ -43,19 +43,9 @@
   </div>
 </template>
 
-<script setup>
-import {
-  ref,
-  reactive,
-  getCurrentInstance,
-  nextTick,
-  computed,
-  onMounted,
-} from "vue";
-const { proxy } = getCurrentInstance();
-import { useRoute, useRouter } from "vue-router";
-const route = useRoute();
-const router = useRouter();
+<script setup lang="ts">
+import { ref, getCurrentInstance, computed, onMounted } from "vue";
+const { proxy } = getCurrentInstance() as any;
 
 const props = defineProps({
   source: {
